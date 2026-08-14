707652c15049733485b28c1d9c514d2b775f5bdd
bitwisp <wangawen85@163.com>
2026-08-14 21:48:49 +0800

    test(cmake): add Catch2 test infrastructure

---
+++ cmake/Dependencies.cmake
@@ -0,0 +1,18 @@
+include(FetchContent)
+
+function(lx_add_test_dependencies)
+    set(CATCH_INSTALL_DOCS OFF CACHE BOOL "" FORCE)
+    set(CATCH_INSTALL_EXTRAS OFF CACHE BOOL "" FORCE)
+
+    FetchContent_Declare(
+        Catch2
+        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+        GIT_TAG v3.15.0
+        GIT_SHALLOW TRUE
+    )
+    FetchContent_MakeAvailable(Catch2)
+
+    list(APPEND CMAKE_MODULE_PATH "${catch2_SOURCE_DIR}/extras")
+    set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" PARENT_SCOPE)
+endfunction()
+
---
+++ tests/unit/smoke_test.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("the test infrastructure is available")
+{
+    REQUIRE(true);
+}
+
